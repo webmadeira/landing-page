aaedacd6ad8f66cc5dbfdbfb0a1c3662b9a3ddd8
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-12-04 11:46:12 +0000

    Migrate footer component

--- src/client/js/components/Footer/Sponsors.js
+++ src/client/js/components/Footer/Sponsors.js
@@ -2,15 +2,17 @@ import React, { PropTypes } from 'react'
 import translate from '../translate'
 
 const Sponsors = props => (
-  <footer>
-    <p>{props.strings.sponsors}</p>
-    <a
-      href="https://www.facebook.com/CasaDoPovoDaSerraDeAgua"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img src="/img/sponsor-serra-de-agua.jpg" alt="logo" />
-    </a>
+  <footer className="o-footer">
+    <div className="o-container">
+      <p className="a-body-2 is-highlight is-cap">{props.strings.title}</p>
+      <a
+        href="https://www.facebook.com/CasaDoPovoDaSerraDeAgua"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/img/sponsor-serra-de-agua.jpg" alt="logo" />
+      </a>
+    </div>
   </footer>
 )
 
