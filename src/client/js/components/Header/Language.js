563dfa5f0b4ad07606c29c5264e741e44a78628e
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-12-04 10:45:12 +0000

    Change language component

--- src/client/js/components/Header/Language.js
+++ src/client/js/components/Header/Language.js
@@ -3,10 +3,10 @@ import translate from '../translate'
 
 // TODO: Map each language
 const Language = props => (
-  <nav>
+  <nav className="o-container">
     <ul className="m-lang-selector">
-      <li><a href="#en" className="a-lang" onClick={() => props.onLanguageChange('en')}>{props.strings.en}</a></li>
-      <li><a href="#pt" className="a-lang" onClick={() => props.onLanguageChange('pt')}>{props.strings.pt}</a></li>
+      <li><a href="#en" className="a-lang a-link" onClick={() => props.onLanguageChange('en')}>{props.strings.en}</a></li>
+      <li><a href="#pt" className="a-lang a-link" onClick={() => props.onLanguageChange('pt')}>{props.strings.pt}</a></li>
     </ul>
   </nav>
 )
