219e088ff41fa54413f854c6dae887f84d3bffb6
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-12-04 11:05:33 +0000

    Migrate header's center content

--- src/client/js/components/Header/CenteredBox.js
+++ src/client/js/components/Header/CenteredBox.js
@@ -5,10 +5,10 @@ const Headline = (props) => {
   const { title, description, location } = props.strings
 
   return (
-    <section>
-      <header>{title}</header>
-      <p>{description}</p>
-      <footer>{location}</footer>
+    <section className="m-header-centered-box">
+      <header className="a-headline">{title}</header>
+      <p className="a-subheading">{description}</p>
+      <footer className="a-subheading is-highlight">{location}</footer>
     </section>
   )
 }
